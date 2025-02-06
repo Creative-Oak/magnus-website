995bc2d6cac5dfab5f5141efe0897608bf0efd31
Magnus H Kaspersen <magnushoholt@pm.me>
2025-02-06 11:46:40 +0100

    added og image

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ComponentChildren, h } from "preact";
+import { Head } from "$fresh/runtime.ts";
 import { NavigationMenu } from "../islands/NavigationMenu.tsx";
 import { Footer } from "../components/Footer.tsx";
 
@@ -7,9 +8,24 @@ interface LayoutProps {
   title: string;
 }
 
+const baseUrl = Deno.env.get("ENVIRONMENT") === "production"
+  ? "https://magnushkaspersen.com"
+  : "http://localhost:8000";
+
 export function Layout({ children, title }: LayoutProps) {
   return (
     <>
+      <Head>
+        <meta property="og:image" content={`${baseUrl}/images/og-image.webp`} />
+        <meta
+          property="og:title"
+          content="Magnus H. Kaspersen. AI-workshops, -foredrag og -konsulentbistand"
+        />
+        <meta
+          property="og:description"
+          content="Professionel foredragsholder og AI-workshop facilitator, der hjælper organisationer med at navigere i teknologiens fremtid gennem engagerende præsentationer og praktiske workshops."
+        />
+      </Head>
       <NavigationMenu />
       <main class="min-h-screen bg-white">
         {children}
